2b4fdae82e6056b7b4d71502b4dd8b185e8051fa
Tibo <tibogeysen@hotmail.com>
2019-04-15 22:01:08 +0200

    footer & header design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { FooterComponent } from './components/footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
